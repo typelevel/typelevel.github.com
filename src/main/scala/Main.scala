9dbe086b68c9c148283f368a3394bb2c84d0e36d
Lars Hupel <lars.hupel@mytum.de>
2017-12-17 22:51:19 +0100

    tut execution can be parallelized

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,13 +1,19 @@
 package org.typelevel.blog
 
+import scala.concurrent.{Await, Future}
+import scala.concurrent.duration._
 import org.yaml.snakeyaml.Yaml
 
 object Main extends App {
+
+  import scala.concurrent.ExecutionContext.Implicits.global
 
   val posts = BuildInfo.tutInput.listFiles().toList.filter { file =>
     file.isFile() && file.getName.endsWith(".md")
   }.map(Post(_))
 
-  posts.foreach(_.process())
+  val future = Future.traverse(posts)(_.process)
+
+  Await.result(future, Duration.Inf)
 
 }
